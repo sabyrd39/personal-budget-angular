74c02552d66580a8988251e938a1f331ed3b48ff
sabyrd39 <sbyrd39@uncc.edu>
2020-10-04 19:58:22 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,22 +4,7 @@ const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-    ]
-};
+const budget = require("./required.json");
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
